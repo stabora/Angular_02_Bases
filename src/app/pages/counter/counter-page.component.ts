9eec415aac2dc7ae94e6b5299325e53bbbe42834
stabora <stabora@vanzai.com.ar>
2026-01-27 22:29:36 -0300

    Se agregan ejercicios de la Sección 4 - Bases de Angular

---
+++ src/app/pages/counter/counter-page.component.ts
@@ -0,0 +1,27 @@
+import { ChangeDetectionStrategy, Component, signal } from "@angular/core";
+
+@Component({
+    templateUrl: './counter-page.component.html',
+    styleUrl: './counter-page.component.css'
+})
+export class CounterPageComponent {
+    counter: number = 10;
+    counterSignal = signal(10);
+
+    constructor() {
+        setInterval(() => {
+            this.counterSignal.update((value) => value += 1);
+            console.log('Tick');
+        }, 2000);
+    }
+
+    updateCounter(value: number) {
+        this.counter += value;
+        this.counterSignal.update((current) => current + value);
+    }
+
+    resetCounter() {
+        this.counter = 0;
+        this.counterSignal.set(0)
+    }
+}
---
+++ src/app/pages/hero/hero-page.component.ts
@@ -0,0 +1,34 @@
+import { UpperCasePipe } from "@angular/common";
+import { Component, computed, signal } from "@angular/core";
+
+@Component({
+    templateUrl: 'hero-page.component.html',
+    styleUrl: 'hero-page.component.css',
+    imports: [UpperCasePipe]
+})
+export class HeroPageComponent {
+    name = signal('Ironman');
+    age = signal(45);
+
+    heroDescription = computed(() => `${this.name()} - ${this.age()}`);
+
+    capitalizedName = computed(() => this.name().toUpperCase());
+
+    getHeroDescription() {
+        return `${this.name()} - ${this.age()}`;
+    }
+
+    changeHero() {
+        this.name.set('Spiderman');
+        this.age.set(22);
+    }
+
+    resetForm() {
+        this.name.set('Ironman');
+        this.age.set(45);
+    }
+
+    changeAge() {
+        this.age.set(60);
+    }
+}
